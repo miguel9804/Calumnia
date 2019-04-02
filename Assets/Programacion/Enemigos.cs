15f7238f77708eef7ae0ff66f2f1c69a93c39ece
MiguelVargas <miguelangel9633@hotmail.com>
2019-04-02 08:01:10 -0500

    Segunda version del juego

--- Assets/Programacion/Enemigos.cs
+++ Assets/Programacion/Enemigos.cs
@@ -8,7 +8,7 @@ public class Enemigos : MonoBehaviour
     private int indice;
     private Vector3 pos_siguiente;
     public float vel;
-    private float dis_punto = 0.5f;
+    private float dis_punto = 0.5f ;
     [SerializeField]
     private int vida;
     private const string FLECHA = "Flecha";
@@ -16,6 +16,7 @@ public class Enemigos : MonoBehaviour
     private const string AURA = "Aura";
     [SerializeField]
     private Transform cementerio;
+    
 
     void Start()
     {
@@ -28,15 +29,25 @@ public class Enemigos : MonoBehaviour
     {
         Vector3 dir = pos_siguiente - this.transform.position;
         this.transform.position += dir * vel * Time.deltaTime;
-        if (dir.magnitude <= dis_punto)
-        {
-            if (indice + 1 < ruta.transform.childCount)
+        //this.transform.position = this.transform.position+("")
+  
+            if (dir.magnitude <= dis_punto)
             {
-                indice++;
-                pos_siguiente = ruta.transform.GetChild(indice).position;
-                //Debug.Log("xs= " + pos_siguiente.x + ", ys= " + pos_siguiente.y);
+                if (indice + 1 < ruta.transform.childCount)
+                {
+                    indice++;
+                    pos_siguiente = ruta.transform.GetChild(indice).position;
+                    //Debug.Log("xs= " + pos_siguiente.x + ", ys= " + pos_siguiente.y);
+                }
+                else
+                {
+                    indice = 0;
+                    this.transform.position = pos_siguiente;
+                    pos_siguiente = ruta.transform.GetChild(0).position;
+                }
             }
-        }
+       
+        
     }
     void Muerte()
     {
@@ -73,4 +84,6 @@ public class Enemigos : MonoBehaviour
         }
         
     }
+    
+    
 }
--- Assets/Programacion/Oleadas.cs
+++ Assets/Programacion/Oleadas.cs
@@ -10,8 +10,10 @@ public class Oleadas : MonoBehaviour {
     [SerializeField]
     private GameObject enemigo2;
     [SerializeField]
+    private GameObject enemigo3;
+    [SerializeField]
     private Transform spawn;
-    private int waves = 4;
+    private int waves = 5;
     private int contador = 0;
     private int terminar_nivel;
     [SerializeField]
@@ -20,20 +22,23 @@ public class Oleadas : MonoBehaviour {
     void Oleada1()
     {
         GameObject temp;
-        Vector3 pos_inicial = spawn.transform.position;
-        Vector3 incremento = new Vector3(0, 5);
-        for (int i = 0; i < 2; i++)
+        Vector3 pos_inicial = spawn.position;
+        Vector3 incremento = new Vector3(0, -5);
+        Vector3 n = new Vector3(0, -10);
+        for(int i=0;i<2;i++)
         {
             temp = Instantiate(enemigo1, pos_inicial + incremento, Quaternion.identity);
             pos_inicial = temp.transform.position;
             enemigos.Add(temp);
         }
+            
+        
     }
     void Oleada2()
     {
         GameObject temp;
-        Vector3 pos_inicial = spawn.transform.position;
-        Vector3 incremento = new Vector3(0, 5);
+        Vector3 pos_inicial = spawn.position;
+        Vector3 incremento = new Vector3(0, -5);
         for (int i = 0; i < 4; i++)
         {
             temp = Instantiate(enemigo1, pos_inicial + incremento, Quaternion.identity);
@@ -45,8 +50,8 @@ public class Oleadas : MonoBehaviour {
     {
         GameObject temp;
         GameObject temp2;
-        Vector3 pos_inicial = spawn.transform.position;
-        Vector3 incremento = new Vector3(0,5); 
+        Vector3 pos_inicial = spawn.position;
+        Vector3 incremento = new Vector3(0,-5); 
         for(int i =0; i<4;i++)
         {
             temp = Instantiate(enemigo1, pos_inicial + incremento, Quaternion.identity);
@@ -66,8 +71,8 @@ public class Oleadas : MonoBehaviour {
     {
         GameObject temp;
         GameObject temp2;
-        Vector3 pos_inicial = spawn.transform.position;
-        Vector3 incremento = new Vector3(0, 5);
+        Vector3 pos_inicial = spawn.position;
+        Vector3 incremento = new Vector3(0, -5);
         for (int i = 0; i < 6; i++)
         {
             temp = Instantiate(enemigo1, pos_inicial + incremento, Quaternion.identity);
@@ -83,54 +88,77 @@ public class Oleadas : MonoBehaviour {
         }
 
     }
-
-	IEnumerator spawnOleadas()
+    void Oleada5()
     {
-        contador++;
-        Debug.Log("Empieza la oleada:"+contador);
+        GameObject temp;
+        GameObject temp2;
+        GameObject temp3;
+        Vector3 pos_inicial = spawn.position;
+        Vector3 incremento = new Vector3(0, -5);
+        for (int i = 0; i < 4; i++)
+        {
+            temp = Instantiate(enemigo1, pos_inicial + incremento, Quaternion.identity);
+            pos_inicial = temp.transform.position;
+            enemigos.Add(temp);
+            if (i >= 2)
+            {
+                temp2 = Instantiate(enemigo2, pos_inicial + incremento, Quaternion.identity);
+                pos_inicial = temp2.transform.position;
+                enemigos.Add(temp2);
+                
+            }
+            if (i == 3)
+            {
+                temp3 = Instantiate(enemigo3, pos_inicial + incremento, Quaternion.identity);
+                pos_inicial = temp3.transform.position;
+                enemigos.Add(temp3);
+            }
 
 
-        if (contador == 1)
-        {
-            Oleada1();
-            yield return new WaitForSeconds(2f);
         }
-        else if(contador==2)
-        {
-            Oleada2();
-            yield return new WaitForSeconds(2f);
-        }
-        else if(contador==3)
-        {
-            Oleada3();
-            yield return new WaitForSeconds(2f);
-        }
-        else if(contador==4)
-        {
-            Oleada4();
-            yield return new WaitForSeconds(2f);
-        }
+
     }
-	
-	// Update is called once per frame
-	void Update () {
+
+
+
+    // Update is called once per frame
+    void Update () {
 
         if(Input.GetKeyDown(KeyCode.Space))
         {
+            contador++;
+            terminar_nivel++;
+            Debug.Log(contador);
             Destroy(space.gameObject);
-            if(contador<=waves)
+            if (contador == 1)
             {
-                StartCoroutine(spawnOleadas());
-                terminar_nivel++;
+                Oleada1();
+                
             }
-            else
+            else if (contador == 2)
             {
-                StopCoroutine(spawnOleadas());
-                if (terminar_nivel > waves)
-                {
+                Oleada2();
+                
+            }
+            else if (contador == 3)
+            {
+                Oleada3();
+                
+            }
+            else if (contador == 4)
+            {
+                Oleada4();
+                
+            }
+            else if ( contador==5)
+            {
+                Oleada5();
+            }
+             if (terminar_nivel > waves)
+             {
                     SceneManager.LoadScene("Ganar");
-                }
-            }
+             }
+            
             
             
         }
